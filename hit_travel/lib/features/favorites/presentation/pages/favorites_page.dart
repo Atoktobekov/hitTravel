96c3a0442c967b0dcdbe7db6d0fbec9822c385ae
Adi <atoktobekov1@gmail.com>
2025-12-26 17:14:01 +0600

    feat: update UI for Favorites and My Bookings placeholder pages
    - Set up styled `AppBar` for `FavoritesPage` and `MyBookingsPage` using `AppTheme.blueColor`.
    - Add localized titles and centered placeholder text for both pages.

--- hit_travel/lib/features/favorites/presentation/pages/favorites_page.dart
+++ hit_travel/lib/features/favorites/presentation/pages/favorites_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hit_travel/core/theme/theme.dart';
 
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
@@ -6,8 +7,27 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const Center(child: Text('Home Page')),
+      appBar: AppBar(
+        title: Text(
+          'Избранные',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: AppTheme.blueColor,
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Text(
+            'Привет! Раздел находится в разработке, скоро тут будут ваши избранные.',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+        ),
+      ),
     );
   }
 }
--- hit_travel/lib/features/my_bookings/presentation/pages/my_bookings_page.dart
+++ hit_travel/lib/features/my_bookings/presentation/pages/my_bookings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hit_travel/core/theme/theme.dart';
 
 class MyBookingsPage extends StatelessWidget {
   const MyBookingsPage({super.key});
@@ -6,8 +7,27 @@ class MyBookingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const Center(child: Text('Home Page')),
+      appBar: AppBar(
+        title: Text(
+          'Мои заказы',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: AppTheme.blueColor,
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Text(
+            'Привет! Раздел находится в разработке, скоро тут будут ваши заказы.',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+        ),
+      ),
     );
   }
 }
